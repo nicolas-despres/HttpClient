deba7e07aad405f9fa8703cf599698e49b5ea782
Nicolas Despres <nicolas.despres@mphasis.com>
2021-08-31 17:53:49 +0200

    fix: node service

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const http = require("http")
 const host = 'localhost'
 const port = 8080
 
-const requestListener = function (req, res) {}
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
 
 const server = http.createServer(requestListener)
 server.listen(port, host, () => {
